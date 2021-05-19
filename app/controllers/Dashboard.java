730c81f966b16fb9bb5d9fe36b5af7d865256abc
arc_85 <andrewrosscameron@gmail.com>
2021-05-19 23:07:52 +0100

    more formatting and overcoming error with routes favicon

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -9,35 +9,31 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class Dashboard extends Controller
-{
-  public static void index()
-  {
-    Logger.info("Rendering Dashboard");
-    Member member = Accounts.getLoggedInMember();
-    List<Station> stations = member.stations;
-    Collections.sort(stations, Comparator.comparing(Station::getName));
-    render("dashboard.html", member, stations);
-  }
+public class Dashboard extends Controller {
+    public static void index() {
+        Logger.info("Rendering Dashboard");
+        Member member = Accounts.getLoggedInMember();
+        List<Station> stations = member.stations;
+        Collections.sort(stations, Comparator.comparing(Station::getName));
+        render("dashboard.html", member, stations);
+    }
 
-  public static void addStation(String name, double lat, double lng)
-  {
-    Member member = Accounts.getLoggedInMember();
-    Station stations = new Station(name, lat, lng);
-    member.stations.add(stations);
-    member.save();
-    Logger.info("Addint Station" + name);
-    redirect("/dashboard");
-  }
+    public static void addStation(String name, double lat, double lng) {
+        Member member = Accounts.getLoggedInMember();
+        Station stations = new Station(name, lat, lng);
+        member.stations.add(stations);
+        member.save();
+        Logger.info("Addint Station" + name);
+        redirect("/dashboard");
+    }
 
-  public static void deleteStation(Long id, Long stationid)
-  {
-    Member member = Member.findById(id);
-    Station stations = Station.findById(stationid);
-    member.stations.remove(stations);
-    member.save();
-    stations.delete();
-    Logger.info("Deleting " + stations.name);
-    redirect("/dashboard");
-  }
+    public static void deleteStation(Long id, Long stationid) {
+        Member member = Member.findById(id);
+        Station stations = Station.findById(stationid);
+        member.stations.remove(stations);
+        member.save();
+        stations.delete();
+        Logger.info("Deleting " + stations.name);
+        redirect("/dashboard");
+    }
 }
--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -9,27 +9,24 @@ import models.Reading;
 import play.Logger;
 import play.mvc.Controller;
 
-public class StationCtrl extends Controller
-{
-    public static void index(Long id)
-    {
+public class StationCtrl extends Controller {
+    public static void index(Long id) {
         Station station = Station.findById(id);
-        Logger.info ("Station id = " + id);
+        Logger.info("Station id = " + id);
         render("station.html", station);
     }
-    public static void deletereading (Long id, Long readingid)
-    {
+
+    public static void deletereading(Long id, Long readingid) {
         Station station = Station.findById(id);
         Reading reading = Reading.findById(readingid);
-        Logger.info ("Removing Reading id = " + readingid);
+        Logger.info("Removing Reading id = " + readingid);
         station.readings.remove(reading);
         station.save();
         reading.delete();
         render("station.html", station);
     }
 
-    public static void addReading(Long id, int code, double temperature, double windSpeed, double windDirection, int pressure)
-    {
+    public static void addReading(Long id, int code, double temperature, double windSpeed, double windDirection, int pressure) {
         Station station = Station.findById(id);
         Date time = new Date();
         Reading readings = new Reading(time, code, temperature, windSpeed, windDirection, pressure);
